3a7a28e7c8078dd3576bf702ad488c23f2efc22b
Matt Andreko <mandreko@gmail.com>
2018-01-22 14:37:14 -0500

    Added bootstrap and stylization

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/summaries', function(req, res) {
+    const viewings = {
+        2013: [
+            {name: 'matt', total: 325, new: 153, theater: 33},
+            {name: 'mike', total: 173, new: 94, theater: 65},
+            {name: 'tiny', total: 320, new: 158, theater: 25},
+        ],
+        2014: [
+            {name: 'matt', total: 263, new: 124, theater: 50},
+            {name: 'mike', total: 213, new: 104, theater: 64},
+            {name: 'tiny', total: 366, new: 252, theater: 28},
+        ],
+        2015: [
+            {name: 'matt', total: 230, new: 138, theater: 71},
+            {name: 'mike', total: 0, new: 0, theater: 62},
+            {name: 'tiny', total: 103, new: 67, theater: 26},
+        ],
+        2016: [
+            {name: 'matt', total: 176, new: 133, theater: 83},
+            {name: 'mike', total: 147, new: 0, theater: 52},
+            {name: 'tiny', total: 81, new: 0, theater: 16},
+        ],
+        2017: [
+            {name: 'matt', total: 136, new: 66, theater: 40},
+            {name: 'mike', total: 135, new: 0, theater: 51},
+            {name: 'tiny', total: 155, new: 0, theater: 9},
+        ]
+    };
+    res.send(JSON.stringify(viewings));
+});
+
+app.listen(process.env.PORT || 8080);
--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,23 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import logo from '../logo.png';
 import '../style/App.css';
 import ViewingNumberCharts from './viewing_numbers_chart';
 
 class App extends Component {
 
-    render() {
-        const matt = 'Matt';
-        const mike = 'Mike';
-        const tiny = 'Tiny';
+    state = {viewings: ""}
 
-        const viewings2013 = [
-            {name: matt, total: 325, new: 153, theater: 33}, // first year he saw more than Tiny
-            {name: mike, total: 173, new: 94, theater: 65},
-            {name: tiny, total: 320, new: 158, theater: 25},
-        ];
-        const viewings2014 = [
-            {name: matt, total: 263, new: 124, theater: 50},
-            {name: mike, total: 213, new: 104, theater: 64},
-            {name: tiny, total: 366, new: 252, theater: 28},
-        ];
-        const viewings2015 = [
-            {name: matt, total: 230, new: 138, theater: 71},
-            {name: mike, total: 0, new: 0, theater: 62}, // Got married? Maybe 2014
-            {name: tiny, total: 103, new: 67, theater: 26}, // Relationship with Paige
-        ];
-        const viewings2016 = [
-            {name: matt, total: 176, new: 133, theater: 83},
-            {name: mike, total: 147, new: 0, theater: 52}, // Child born, Masters received
-            {name: tiny, total: 81, new: 0, theater: 16}, // Got engaged
-        ];
-        const viewings2017 = [
-            {name: matt, total: 136, new: 66, theater: 40},
-            {name: mike, total: 135, new: 0, theater: 51},
-            {name: tiny, total: 155, new: 0, theater: 9}, // Got married, bought a house, got a dog
-        ];
-        const viewings = {
-            2013: viewings2013,
-            2014: viewings2014,
-            2015: viewings2015,
-            2016: viewings2016,
-            2017: viewings2017,
-        };
+    componentDidMount() {
+        axios.get('/summaries')
+            .then(res => {
+                const viewings = res.data;
+                this.setState({viewings});
+            });
+    }
+
+    render() {
+        const viewings = this.state.viewings;
 
         return (
             <div className="App">
@@ -49,7 +25,6 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Year In Review Stats</h1>
                 </header>
-
                 <div className="container">
                     <div className="row">
                         {Object.keys(viewings).map((key, index) => {

a9364ad0378b8d4499b7618467446567ce787c65
Matt Andreko <mandreko@gmail.com>
2018-01-26 11:09:53 -0500

    Fixed the run job, so that production didn't use nodemon

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('client/build'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));

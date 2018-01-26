33e181da12575d2682fc96796cdd915203e2776d
Matt Andreko <mandreko@gmail.com>
2018-01-26 11:24:59 -0500

    Added gzip compression to Express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const compression = require('compression');
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+app.use(compression());
 //app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static('client/build'));
 

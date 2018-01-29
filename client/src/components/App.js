42a1b7335cafeeeef41c1f9036cc862eb3d2054b
Matt Andreko <mandreko@gmail.com>
2018-01-29 14:22:00 -0500

    Broke out chart loop into a component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,41 +1,18 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import '../style/App.css';
-import _ from 'lodash';
-import ViewingNumberCharts from './viewing_numbers_chart';
+
 import Header from './header';
 import NavBar from './navbar';
+import ViewingNumberChartList from './viewing_numbers_chart_list';
+
 
 class App extends Component {
-
-    state = {viewings: ""}
-
-    componentDidMount() {
-        axios.get('/stats')
-            .then(res => {
-                const viewings = res.data;
-                this.setState({viewings});
-            });
-    }
-
     render() {
-        var grouped = _.groupBy(this.state.viewings, viewing => viewing.Year.name);
-
         return (
             <div className="App">
                 <Header title="Year In Review Stats"/>
-                <NavBar />
-
-
-
-
-                <div className="container">
-                    <div className="row">
-                        {Object.keys(grouped).map((key, index) => {
-                            return <ViewingNumberCharts key={key} data={grouped[key]} title={key} />
-                        })}
-                    </div>
-                </div>
+                <NavBar/>
+                <ViewingNumberChartList />
             </div>
         );
     }
--- client/src/components/viewing_numbers_chart.js
+++ client/src/components/viewing_numbers_chart.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import '../style/viewing_numbers_chart.css';
-import {ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line} from 'recharts';
+import {ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line, ResponsiveContainer} from 'recharts';
+import {Container, Row, Col} from 'reactstrap';
 
 export default (props) => {
     return (
-        <div className="chart col">
-            <div className="chart-title">
-                <h3>{props.title}</h3>
-            </div>
-            <div>
-                <ComposedChart width={440} height={200} data={props.data}
-                          margin={{top: 20, right: 30, left: 20, bottom: 5}}>
-                    <XAxis dataKey="Reviewer.name"/>
-                    <YAxis/>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <Tooltip/>
-                    <Legend />
-                    <Bar dataKey="theaterViews" fill="#FD8604" name="In Theater" />
-                    <Bar dataKey="newViews" fill="#FFB717" name="First Viewing" />
-                    <Line type='monotone' dataKey='totalViews' stroke='#FECF88' name="Total"/>
-                </ComposedChart>
-            </div>
-        </div>
+        <Col xl={4} lg={6} md={12} sm={12}>
+            <Container fluid={true}>
+                <Row>
+                    <Col>
+                        <h3>{props.title}</h3>
+
+                    </Col>
+                </Row>
+                <Row>
+                    <ResponsiveContainer width="100%" height={300}>
+                        <ComposedChart data={props.data} margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+                            <XAxis dataKey="Reviewer.name"/>
+                            <YAxis/>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <Tooltip/>
+                            <Legend/>
+                            <Bar dataKey="theaterViews" fill="#FD8604" name="In Theater"/>
+                            <Bar dataKey="newViews" fill="#FFB717" name="First Viewing"/>
+                            <Line type='monotone' dataKey='totalViews' stroke='#FECF88' name="Total"/>
+                        </ComposedChart>
+                    </ResponsiveContainer>
+                </Row>
+            </Container>
+        </Col>
     )
 }
